56a024ceeb55d56339e44c8fb7dd4a81734b4953
ImJhoon <jhjh9907@gmail.com>
2026-05-21 12:06:42 +0900

    데이터 타입 개념 정리

---
+++ 04_data-type.js
@@ -0,0 +1,54 @@
+// JS -> 값을 분류. 적절한 대응 -> 타입(형) -> data type -> 자료형
+
+// number -> 감싸줄 필요가 없음. '', "", {}, []...
+// namespace -> 숫자로 시작하지 않는다
+const int = 1;
+const int2 = -1;
+const float = 1.01;
+console.log(typeof int);
+console.log(typeof int2);
+console.log(typeof float);
+
+// boolean
+const t = true; // 예약어, 키워드 'true' 변수명 쓸 수 X
+const f = false;
+console.log(typeof t);
+console.log(typeof f);
+
+// string - 열. 현
+// 문자(1글자) -> 메모리. -> 메모리들을 연결시켜서 문자들의 집합/배열/나열 -> 문자배열 {'h', 'e', 'l', 'l', 'o'}
+// 문자열 (String) -> "hello"
+// JavaScript - 'h', 'Hello', -> 'hello', "hello".
+const name = "윌리엄";
+console.log(typeof name);
+
+// null
+const n = null;
+console.log(typeof n); // object 설계 미스
+
+// undefined
+let u;
+console.log(typeof u);
+let u2 = undefined;
+console.log(typeof u2);
+let u3 = {};
+console.log(typeof u3.u);
+
+// object
+let o = {}; // 주소가 저장되는 타입
+console.log(typeof o);
+let arr = [];
+console.log(typeof arr);
+
+// 템플릿 리터럴
+const nn = "김자바";
+console.log(nn + "는 개발자이다.");
+console.log(`${nn}는 개발자이다.`); // 변수를 + 연산을 수행하지 않고 넣을 수 있음
+
+console.log(`시작
+    
+    
+          엔터가 있으면 자동으로 인식
+    
+    
+        끝`); // 들여쓰기 및 스페이스 바도 자동으로 인식
